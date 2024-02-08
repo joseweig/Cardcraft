8f7abfd768dde3b621b07678de4746be11cdf3d1
jmayoral <joseweig@gmail.com>
2024-02-07 21:24:08 -0600

    Remove site.js

--- Cardcraft/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-function selectAllTextOnFocus(elementId) {
-    const inputElement = document.getElementById(elementId);
-    if (inputElement) {
-        inputElement.addEventListener('focus', function () {
-            inputElement.select();
-        });
-    }
-}
